717aa873165bb4f3ee13b842ca626d81c2efd27c
imade osifo <imadejr@yahoo.com>
2019-10-30 18:25:49 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count = 0 
+    
+    words = self.split(" ")
+    
+    words.each do |word|
+      if word.include
+    end 
   end
 end
